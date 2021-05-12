d9df5421a28bebba801b1d956f5b5059a8f32b48
QuimRobres <quim.robres@gmail.com>
2021-05-12 16:26:05 +0200

    user and service CRUD

--- .history/routes/public.routes_20210512162545.js
+++ .history/routes/public.routes_20210512162545.js
@@ -9,6 +9,6 @@ router.get("/services", (req, res, next) => {
     Service.find({})
     .then(services => res.status(200).json(todos))
     .catch(err => res.status(500).json(err))
-});
+})
 
 module.exports = router;

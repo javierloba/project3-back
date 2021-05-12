8cc606a8fe5fe3992cc522ff99d9ae8f7be4841a
Esther Perez Serrajordi <estherperezserrajordi@MacBook-Pro-de-Esther.local>
2021-05-12 17:32:03 +0200

    Nodemailer implementation in create client

---
+++ configs/nodemailer.config.js
@@ -0,0 +1,8 @@
+const nodemailer = require('nodemailer');
+module.exports = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+})

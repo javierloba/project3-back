d6826f1160527b8e77a7c361109d0ab0e724ac21
QuimRobres <quim.robres@gmail.com>
2021-05-17 15:21:09 +0200

    fixed nodemailer

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,12 +2,19 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const User = require("../models/User.model");
-const Worker = require("../models/Worker.model")
-const transporter = require('../configs/nodemailer.config');
+const Worker = require("../models/Worker.model");
+const transporter = require("../configs/nodemailer.config");
 const bcrypt = require("bcryptjs");
 const bcryptSalt = 10;
-const { checkRole } = require('../middlewares/index')
+const { checkRole } = require("../middlewares/index");
 
+var message = {
+  from: "sender@server.com",
+  to: "receiver@sender.com",
+  subject: "Message title",
+  text: "Plaintext version of the message",
+  html: "<p>HTML version of the message</p>",
+};
 //Create user -------- OK
 router.post("/createUser", (req, res, next) => {
   const {
@@ -17,15 +24,13 @@ router.post("/createUser", (req, res, next) => {
     birthday,
     phone_number,
     email,
-    password
+    password,
   } = req.body;
-  console.log(req.body)
+  console.log(req.body);
   if (password.length < 3) {
-    return res
-      .status(400)
-      .json({
-        message: "Please make your password at least 3 characters long",
-      });
+    return res.status(400).json({
+      message: "Please make your password at least 3 characters long",
+    });
   }
 
   if (
@@ -42,101 +47,89 @@ router.post("/createUser", (req, res, next) => {
       .json({ message: "Please fill all the fields in the form" });
   }
 
-  User.findOne({ email }).then((user) => {
-    if (user) {
-      return res
-        .status(400)
-        .json({ message: "User already exists. Please change your email" });
-    }
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        return res
+          .status(400)
+          .json({ message: "User already exists. Please change your email" });
+      }
 
-    const salt = bcrypt.genSaltSync(bcryptSalt);
-    const hashPass = bcrypt.hashSync(password, salt);
+      const salt = bcrypt.genSaltSync(bcryptSalt);
+      const hashPass = bcrypt.hashSync(password, salt);
 
-    User.create({
-      client_number,
-      name,
-      surname,
-      birthday,
-      phone_number,
-      email,
-      password: hashPass,
-    })
-      .then((newUser) => {
-        console.log("usuario creado")  
-        if (error) {
-            return res.status(500).json({error: "error linea 125"});
-          }
-            transporter.sendMail({
-              from: "Iron Nails <ironhacknails@gmail.com>",
+      User.create({
+        client_number,
+        name,
+        surname,
+        birthday,
+        phone_number,
+        email,
+        password: hashPass,
+      })
+        .then((newUser) => {
+          transporter
+            .sendMail({
+              from: "ironhacknails@gmail.com",
               to: email,
               subject: "Bienvenido a Iron Nails!",
-              html:`<p>Gracias por tu registro ${name}</p>`
+              html: `<p>Gracias por tu registro ${name}</p>`,
             })
             .then(() => {
-              return res.status(200).json(newUser)
+              console.log("CREADO");
+              return res.status(200).json(newUser);
             })
-            .catch((err) => res.status(500).json({error: "error linea 137", message: error}))
-
-      })
-      .catch((error) => res.status(500).json({error: "error linea 139", message: error}))
-  })
-  .catch((error) => res.status(500).json({error: "error linea 143", message: error}))
+            .catch((err) =>
+              res.status(500).json({ err: "error linea 91", message: err })
+            );
+        })
+        .catch((error) =>
+          res.status(500).json({ error: "error linea 96", message: error })
+        );
+    })
+    .catch((error) =>
+      res.status(500).json({ error: "error linea 83", message: error })
+    );
 });
 
 //Create worker ------ OK
 router.post("/createWorker", (req, res, next) => {
-  const {
-  name,
-  surname,
-  email,
-  password,
-  phone_number,
-  role,
-  } = req.body;
+  const { name, surname, email, password, phone_number, role } = req.body;
 
   if (password.length < 3) {
-  return res
-      .status(400)
-      .json({
+    return res.status(400).json({
       message: "Please make your password at least 3 characters long",
-      });
+    });
   }
 
-  if(
-  !name ||
-  !surname ||
-  !email ||
-  !password ||
-  !phone_number ||
-  !role
-  ) {
-  return res
+  if (!name || !surname || !email || !password || !phone_number || !role) {
+    return res
       .status(400)
       .json({ message: "Please fill all the fields in the form" });
   }
 
   Worker.findOne({ email }).then((worker) => {
-  if (worker) {
+    if (worker) {
       return res
-      .status(400)
-      .json({ message: "Worker already exists. Please change the email"})
-  }
+        .status(400)
+        .json({ message: "Worker already exists. Please change the email" });
+    }
 
-  const salt = bcrypt.genSaltSync(bcryptSalt);
-  const hashPass = bcrypt.hashSync(password, salt);
+    const salt = bcrypt.genSaltSync(bcryptSalt);
+    const hashPass = bcrypt.hashSync(password, salt);
 
-  Worker.create({
+    Worker.create({
       name,
       surname,
       email,
       password: hashPass,
       phone_number,
       role,
-  })
-  .then((newWorker) => res.status(200).json(newWorker))
-  .catch(err => res.status(500).json(err))
-  })
-})
+    })
+      .then((newWorker) => res.status(200).json(newWorker))
+      .catch((err) => res.status(500).json(err));
+  });
+});
 
 //LOGIN ----------- OK
 router.post("/login", (req, res, next) => {
@@ -150,7 +143,7 @@ router.post("/login", (req, res, next) => {
     }
 
     req.login(theUser, (error) => {
-      console.log("Auth routes", theUser)
+      console.log("Auth routes", theUser);
       if (error) {
         return res.status(500).json(error);
       }

64da5547b8d935d824e451976f6d790d30d687de
QuimRobres <quim.robres@gmail.com>
2021-05-19 15:57:55 +0200

    -

---
+++ data/reserve.data.js
@@ -0,0 +1,8 @@
+const reserve = [
+    {reservation_date: '2021-05-01', status: "done"},
+    {reservation_date: '2021-05-02', status: "done"},
+    {reservation_date: '2021-05-28', status: "todo"},
+    {reservation_date: '2021-05-29', status: "todo"},
+]
+
+    module.exports = reserve;
